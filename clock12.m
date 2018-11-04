24e52678af2e9ef4edb7ac833498ae1e73578d3e
Nidhi Kumari <nidhi1691996@outlook.com>
2018-11-04 12:44:00 +0530

    text

--- clock12.m
+++ clock12.m
@@ -10,5 +10,10 @@ axis([-3 5 -3 5]);
 text(2.3, -.2, "seconds");
 text(1.3, -.2, "minutes");
 text(.3, -.2, "hours");
+h=text(2.5, .5, '00');
+h1=text(1.5, .5, '00');
+h2=text(.5, .5, '00');
+
+
 
 
