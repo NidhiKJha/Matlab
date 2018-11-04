907d11d9d2aad65472db4fc60a602e3fb43c795d
Nidhi Kumari <nidhi1691996@outlook.com>
2018-11-04 12:57:00 +0530

    so

--- clock12.m
+++ clock12.m
@@ -13,6 +13,19 @@ text(.3, -.2, "hours");
 h=text(2.5, .5, '00');
 h1=text(1.5, .5, '00');
 h2=text(.5, .5, '00');
+for j=0:24
+    for k=0:60
+        for i=0:60
+            set(h,'string',num2str(i));
+            pause(1);
+            drawnow;
+        end
+        set(h1,'string',num2str(k));
+        drawnow;
+    end
+    set(h2,'string',num2str(j));
+    drawnow;
+end
 
 
 
